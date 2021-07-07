ef7355c10441e230d39e07114a577fc005d06c92
javed-tech <jk2169910@gmail.com>
2021-07-07 17:46:50 +0530

    Updates

---
+++ precache-manifest.a4f7d56372aa783a674aa329265be37a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2b1be0894d4bced6eb66c289674c97",
+    "url": "/Animated-app/index.html"
+  },
+  {
+    "revision": "0915a1640d505f17b0a0",
+    "url": "/Animated-app/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "bddd84d4c790cc0b1b65",
+    "url": "/Animated-app/static/css/main.42fae993.chunk.css"
+  },
+  {
+    "revision": "0915a1640d505f17b0a0",
+    "url": "/Animated-app/static/js/2.dfeb193e.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/Animated-app/static/js/2.dfeb193e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bddd84d4c790cc0b1b65",
+    "url": "/Animated-app/static/js/main.97596a29.chunk.js"
+  },
+  {
+    "revision": "f62f9148ea9b47a35a41",
+    "url": "/Animated-app/static/js/runtime-main.3a90f761.js"
+  },
+  {
+    "revision": "8769942a0ae94ca17d9a25100d5a0fde",
+    "url": "/Animated-app/static/media/w10.8769942a.png"
+  },
+  {
+    "revision": "fff88493f5688e5730616756a0e4c2c6",
+    "url": "/Animated-app/static/media/w12.fff88493.jpg"
+  },
+  {
+    "revision": "c948452b61cca7e4132b948decf800dd",
+    "url": "/Animated-app/static/media/w2.c948452b.png"
+  },
+  {
+    "revision": "6ed36087a47648db51f2d9404324d314",
+    "url": "/Animated-app/static/media/w3.6ed36087.png"
+  },
+  {
+    "revision": "adbdc925137661fc9b8a34dcc508a251",
+    "url": "/Animated-app/static/media/w4.adbdc925.png"
+  },
+  {
+    "revision": "a3af0655fc9a481401550b5be0e86cab",
+    "url": "/Animated-app/static/media/w6.a3af0655.png"
+  }
+]);
